952bf32e8bf0a20d3da4cb9017e67ecd6b178c5e
wesc111 <Werner.schoegler@gmail.com>
2025-11-17 18:16:45 +0100

    first example checked in

---
+++ hello-python-main/README.md
@@ -0,0 +1,8 @@
+# hello-python
+
+A first example for using git for a python project.
+
+The python script can be executed by
+```
+python main.py
+```
---
+++ hello-python-main/main.py
@@ -0,0 +1,9 @@
+# Simple Python script demonstrating basic arithmetic operations
+
+print('Hello world, let\'s do some math!')
+operand1 = 127
+operand2 = 23
+print(f"The sum of {operand1} and {operand2} is {operand1 + operand2}")
+print(f"The product of {operand1} and {operand2} is {operand1 * operand2}")
+print(f"The difference of {operand1} and {operand2} is {operand1 - operand2}")
+print(f"The quotient of {operand1} and {operand2} is {operand1 / operand2}")
